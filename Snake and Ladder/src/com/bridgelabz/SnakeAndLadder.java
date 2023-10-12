64d04f1acab91afca31d2fd1495a45fd638c30f3
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 22:54:04 +0530

    Added : Player Checks Option

--- Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
+++ Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
@@ -3,14 +3,38 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
 
         int initialPosition = 0;
 
         System.out.println("Initial Player Position : " + initialPosition);
         Random random =new Random();
-        int dieRoll = random.nextInt(6);
-        System.out.println("After Rolling the Die " + dieRoll);
+        int dieRoll = random.nextInt(6) + 1;
+        System.out.println("Player rolled the die and got a : " + dieRoll);
 
+
+        int option= random.nextInt(3);
+        System.out.println("Option to be followed : "+ option);
+
+        switch (option) {
+            case 0:
+                System.out.println("No Play" );
+                initialPosition += 0;
+                System.out.println("No moves : " + initialPosition);
+                break;
+            case 1:
+                System.out.println("Ladder");
+                initialPosition += dieRoll;
+                System.out.println("Move ahead by " + initialPosition + " blocks");
+                break;
+            case 2:
+                System.out.println("Snake");
+                initialPosition -= dieRoll;
+                System.out.println("Move back by " + initialPosition + " blocks");
+                break;
+
+        }
+        System.out.println("Players Current Position : " + initialPosition);
     }
 }
