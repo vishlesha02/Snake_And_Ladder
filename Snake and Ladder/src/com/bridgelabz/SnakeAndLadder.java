c6a80dc754d92dd0c0da7a094a0e20ad625afdfb
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 17:51:35 +0530

    Added : Die Roll

--- Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
+++ Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
 
         int initialPosition = 0;
 
-        System.out.println("Player Position : " + initialPosition);
+        System.out.println("Initial Player Position : " + initialPosition);
+        Random random =new Random();
+        int dieRoll = random.nextInt(6);
+        System.out.println("After Rolling the Die " + dieRoll);
+
     }
 }
