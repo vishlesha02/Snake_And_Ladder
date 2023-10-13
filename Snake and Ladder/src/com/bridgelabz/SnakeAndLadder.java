0dd0989f0ce888965aebfd05ba4b210fd252e4a4
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-13 11:51:06 +0530

    Added : Dice Roll Count and Player Position

--- Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
+++ Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
@@ -7,14 +7,15 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
 
         int playerPosition = 0;
+        int count=0;
 
         System.out.println("Initial Player Position : " + playerPosition);
         Random random = new Random();
 
-
         while(playerPosition != 100) {
 
             int dieRoll = random.nextInt(6) + 1;
+            count++;
             System.out.println("Player rolled the die and got a : " + dieRoll);
 
             int option = random.nextInt(3);
@@ -25,7 +26,6 @@ public class SnakeAndLadder {
                     System.out.println("No Play");
                     playerPosition += 0;
                     System.out.println("Players Current Position : " + playerPosition);
-
                     break;
 
                 case 1:
@@ -40,7 +40,6 @@ public class SnakeAndLadder {
                         System.out.println("Move ahead by " + dieRoll + " blocks");
                     }
                     System.out.println("Players Current Position : " + playerPosition);
-
                     break;
 
                 case 2:
@@ -52,11 +51,11 @@ public class SnakeAndLadder {
                     }
                     System.out.println("Move back by " + dieRoll + " blocks");
                     System.out.println("Players Current Position : " + playerPosition);
-                    
                     break;
 
             }
-
+            System.out.println("-------------------------------------");
         }
+        System.out.println("Die rolled to win the game : " + count + " times");
     }
 }
