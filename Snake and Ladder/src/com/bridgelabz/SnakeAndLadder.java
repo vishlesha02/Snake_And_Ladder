b71f4745231d7def29318ae918c41af1f2fcc027
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-25 11:51:36 +0530

    Added : Two Players

--- Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
+++ Snake and Ladder/src/com/bridgelabz/SnakeAndLadder.java
@@ -7,55 +7,123 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
 
         int playerPosition = 0;
+        int playerPosition2 = 0;
         int count=0;
 
-        System.out.println("Initial Player Position : " + playerPosition);
+        boolean player1Won = false;
+        boolean player2Won = false;
+
+        System.out.println("Initial 1st Player  Position : " + playerPosition);
+        System.out.println("Initial 2nd Player  Position : " + playerPosition2);
+
         Random random = new Random();
 
-        while(playerPosition != 100) {
+        while (!player1Won && !player2Won) {
 
-            int dieRoll = random.nextInt(6) + 1;
-            count++;
-            System.out.println("Player rolled the die and got a : " + dieRoll);
+            for (int i = 1; i <= 2; i++) {
 
-            int option = random.nextInt(3);
-            System.out.println("Option to be followed : " + option);
+                if(i == 1)
+                {
+                    System.out.println("1st Player ");
+                }
+                else
+                {
+                    System.out.println("2nd Player");
+                }
 
-            switch (option) {
-                case 0:
-                    System.out.println("No Play");
-                    playerPosition += 0;
-                    System.out.println("Players Current Position : " + playerPosition);
+                int dieRoll = random.nextInt(6) + 1;
+                count++;
+                System.out.println("Player rolled the die and got a : " + dieRoll);
+
+                int option = random.nextInt(3);
+                System.out.println("Option to be followed : " + option);
+
+                switch (option) {
+                    case 0:
+                        if(i == 1) {
+                            System.out.println("No Play");
+                            playerPosition += 0;
+                            System.out.println("1st Player Current Position : " + playerPosition);
+                        }
+                        else {
+                            System.out.println("No Play");
+                            playerPosition2 += 0;
+                            System.out.println("2nd Player Current Position : " + playerPosition2);
+                        }
+                        break;
+
+                    case 1:
+                        if(i == 1) {
+                            System.out.println("Ladder");
+                            int newPosition = playerPosition + dieRoll;
+                            if (newPosition > 100) {
+                                System.out.println("Cannot move ahead, You rolled too high");
+                            } else {
+                                playerPosition = newPosition;
+                                System.out.println("Move ahead by " + dieRoll + " blocks");
+
+                                if(newPosition == playerPosition + dieRoll)
+                                {
+                                    System.out.println("Player gets extra chance due to ladder");
+                                    i--;
+                                }
+                            }
+                            System.out.println("1st Player Current Position : " + playerPosition);
+                        }
+                        else {
+                            System.out.println("Ladder");
+                            int newPosition = playerPosition2 + dieRoll;
+                            if (newPosition > 100) {
+                                System.out.println("Cannot move ahead, You rolled too high");
+                            } else {
+                                playerPosition2 = newPosition;
+                                System.out.println("Move ahead by " + dieRoll + " blocks");
+
+                                if(newPosition == playerPosition2 + dieRoll)
+                                {
+                                    System.out.println("Player gets extra chance due to ladder");
+                                    i--;
+                                }
+                            }
+                            System.out.println("2nd Player Current Position : " + playerPosition2);
+                        }
+                        break;
+
+                    case 2:
+                        if(i == 1) {
+                            System.out.println("Snake");
+                            playerPosition -= dieRoll;
+                            if (playerPosition < 0) {
+                                playerPosition = 0;
+                            }
+                            System.out.println("Move back by " + dieRoll + " blocks");
+                            System.out.println("1st Player Current Position : " + playerPosition);
+                        }
+                        else {
+                            System.out.println("Snake");
+                            playerPosition2 -= dieRoll;
+                            if (playerPosition2 < 0) {
+                                playerPosition2 = 0;
+                            }
+                            System.out.println("Move back by " + dieRoll + " blocks");
+                            System.out.println("2nd Player Current Position : " + playerPosition2);                        }
+                        break;
+
+                }
+                System.out.println("-------------------------------------");
+
+                if (playerPosition == 100) {
+                    System.out.println("Player 1 wins!");
+                    player1Won = true;
                     break;
-
-                case 1:
-                    System.out.println("Ladder");
-                    int newPosition = playerPosition + dieRoll;
-                    if(newPosition > 100)
-                    {
-                        System.out.println("Cannot move ahead, You rolled too high");
-                    }
-                    else {
-                        playerPosition = newPosition;
-                        System.out.println("Move ahead by " + dieRoll + " blocks");
-                    }
-                    System.out.println("Players Current Position : " + playerPosition);
+                } else if (playerPosition2 == 100) {
+                    System.out.println("Player 2 wins!");
+                    player2Won = true;
                     break;
-
-                case 2:
-                    System.out.println("Snake");
-                    playerPosition -= dieRoll;
-                    if(playerPosition < 0)
-                    {
-                        playerPosition = 0;
-                    }
-                    System.out.println("Move back by " + dieRoll + " blocks");
-                    System.out.println("Players Current Position : " + playerPosition);
-                    break;
-
+                }
             }
-            System.out.println("-------------------------------------");
         }
+
         System.out.println("Die rolled to win the game : " + count + " times");
     }
 }
